434b4ea601340365ff3f3cf0a3951063ad1b1b5f
Yuumi <yuuming.n@gmail.com>
2018-10-30 22:28:17 -0700

    Make another component timer02

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Timer from "./timer";
+import Timer02 from "./timer02";
 
 export default class App extends Component {
   render() {
     return (
       <div>React simple Timer!
       <Timer />
+      <Timer02 />
       </div>
     );
   }
---
+++ src/components/timer02.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+
+export default class Timer02 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentTime: null, currentDay: null };
+    this.daysArray = [
+      "sunday",
+      "monday",
+      "tuesday",
+      "wednesday",
+      "thursday",
+      "friday",
+      "saturday",
+      "sunday"
+    ];
+  }
+
+  componentWillMount() {
+    this.getCurrentTime();
+  }
+
+  getCurrentTime = () => {
+    let hour = new Date().getHours();
+    let minutes = new Date().getMinutes();
+    let seconds = new Date().getSeconds();
+    let am_pm = "pm";
+
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    if (hour > 12) {
+      hour = hour - 12;
+    }
+
+    if (hour == 0) {
+      hour = 12;
+    }
+
+    if (new Date().getHours() < 12) {
+      am_pm = "am";
+    }
+
+    this.setState({
+      currentTime: hour + ":" + minutes + ":" + seconds + " " + am_pm
+    });
+
+    this.daysArray.map((item, key) => {
+      if (key == new Date().getDay()) {
+        this.setState({ currentDay: item.toUpperCase() });
+      }
+    });
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.getCurrentTime();
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.currentDay}
+        {this.state.currentTime}
+      </div>
+    );
+  }
+}
